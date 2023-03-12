61b49ec9cb0b95f5caa1bc80d10a59cc73e7f599
dimfurman <99078100+dimfurman@users.noreply.github.com>
2023-03-12 13:51:15 +0300

    done

--- script.test.js
+++ script.test.js
@@ -4,7 +4,7 @@ test('full format UTC', () => {
   expect(format_date('Sun Mar 12 2023 13:20:01 GMT+0300')).toBe('12.03.2023');
 });
 
-test('getTime', () => {
+test('miliseconds', () => {
     expect(format_date('1678616401000')).toBe('12.03.2023');
   });
 
